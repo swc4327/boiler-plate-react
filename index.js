05e26c701b6aa442b79daf83b76ad9120a51f01e
unknown <swc4327@naver.com>
2022-02-16 08:34:07 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const bodyParser = require("body-parser");
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json
+app.use(bodyParser.json());
+
 const mongoose = require("mongoose");
 mongoose
   .connect(
-    "mongodb+srv://swc4327:zz33021614!!@boilerplate.mwty9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    config.mongoURI,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -15,6 +25,19 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.post("/register", (req, res) => {
+  //회원가입 할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어줌
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  }); //몽고디비 메소드
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-//npm run start
+/* npm run start */
