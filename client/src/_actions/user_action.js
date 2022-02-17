7063092a07a17bbf7d46f71f095bd1d90882c2cd
unknown <swc4327@naver.com>
2022-02-17 12:31:50 +0900

    client 기능 구현

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { LOGIN_USER } from './types';
+import { LOGIN_USER, REGISTER_USER } from './types';
 
 export function loginUser(dataTosubmit) {
   const request = axios
@@ -13,3 +13,17 @@ export function loginUser(dataTosubmit) {
         payload: request
     }
 }
+
+export function RegisterUser(dataTosubmit) {
+  const request = axios
+    .post("/api/users/register", dataTosubmit)
+    .then(response => response.data )
+
+
+    //리듀서로 보냄
+    return {
+        type: REGISTER_USER,
+        payload: request
+    }
+}
+
--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,9 +1,92 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { loginUser, RegisterUser } from "../../../_actions/user_action";
 
-function RegisterPage() {
+function RegisterPage(props) {
+  const dispatch = useDispatch();
+
+  const [Email, setEmail] = useState("");
+  const [Name, setName] = useState("");
+  const [Password, setPassword] = useState("");
+  const [ConfirmPassword, setConfirmPassword] = useState("");
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+
+  const onNameHandler = (event) => {
+    setName(event.target.value);
+  };
+
+  const onPasswordHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const onConfirmPasswordHandler = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault(); //버튼 누를때 마다 화면 refresh 되는거 막기
+
+    if (Password !== ConfirmPassword) {
+      return alert("비밀번호와 비밀번호 확인은 같아야 합니다.");
+    }
+
+    let body = {
+      email: Email,
+      name: Name,
+      password: Password,
+    };
+
+    // dispatch로  action 날림. actions.user_action (여기서 서버 request, response), 결과(request)를 리듀서로 보냄.
+    // reducer가 상태 변경해서 return
+
+    dispatch(RegisterUser(body)).then((response) => {
+      if (response.payload.success) {
+        props.history.push("/login");
+      } else {
+        alert("Failed to register");
+      }
+    });
+  };
+
   return (
-    <div>RegisterPage</div>
-  )
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+
+        <label>Name</label>
+        <input type="text" value={Name} onChange={onNameHandler} />
+
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          value={ConfirmPassword}
+          onChange={onConfirmPasswordHandler}
+        />
+
+        <br />
+
+        <button>Register</button>
+      </form>
+    </div>
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
